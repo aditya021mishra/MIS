70fbed32ff82e7aa877ef3701c21bf6fabf9481e
aditya021mishra <84763778+aditya021mishra@users.noreply.github.com>
2021-12-13 17:15:36 +0530

    public commit

---
+++ public/script.js
@@ -0,0 +1,54 @@
+// $(document).ready(function(){
+
+//     $('#menu').click(function(){
+//         $(this).toggleClass('fa-times');
+//         $('.navbar').toggleClass('nav-toggle');
+//     });
+
+//     $(window).on('load scroll',function(){
+//         $('#menu').removeClass('fa-times');
+//         $('.navbar').removeClass('nav-toggle');
+//     });
+
+// });
+
+
+// <!----login---->
+// const loginText = document.querySelector(".title-text .login");
+// const loginForm = document.querySelector("form.login");
+// const loginBtn = document.querySelector("label.login");
+// const signupBtn = document.querySelector("label.signup");
+// const signupLink = document.querySelector("form .signup-link a");
+// signupBtn.onclick = (()=>{
+//   loginForm.style.marginLeft = "-50%";
+//   loginText.style.marginLeft = "-50%";
+// });
+// loginBtn.onclick = (()=>{
+//   loginForm.style.marginLeft = "0%";
+//   loginText.style.marginLeft = "0%";
+// });
+// signupLink.onclick = (()=>{
+//   signupBtn.click();
+//   return false;
+// });
+
+
+
+// <!----ends--->
+
+// const readMoreBtn = document.querySelector(".read-more-btn");
+// const text=document.querySelector(".text");
+
+// readMoreBtn.addEventListener("click",(e)=> {
+//     text.classList.toggle("show-more");
+//       if(readMoreBtn.innerText === "Read More"){
+//               readMoreBtn.insertText = "Read Less";
+//     } else{
+//         readMoreBtn.insertText = "Read More";
+//     }
+// });
+
+
+
+
+
